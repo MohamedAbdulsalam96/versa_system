d58a191bd9a9db72787ce12708614bedb373eb25
NaseeraVP <naseeravp38@gmail.com>
2024-10-30 17:00:00 +0530

    fix:Remove the unnecessary documentation

--- versa_system/versa_system/doctype/final_design/final_design.js
+++ versa_system/versa_system/doctype/final_design/final_design.js
@@ -1,13 +1,7 @@
 frappe.ui.form.on('Final Design', {
     refresh: function(frm) {
-        // Check if the workflow state is "Approved"
         if (!frm.is_new() && frm.doc.workflow_state === 'Approved') {
             frm.add_custom_button(__('Sales Order'), function() {
-                // Call the mapping function for Final Design to Sales Order
-                frappe.model.open_mapped_doc({
-                    method: 'versa_system.versa_system.doctype.final_design.final_design.map_final_design_to_sales_order',
-                    frm: frm
-                });
             }, __('Create'));
         }
     }

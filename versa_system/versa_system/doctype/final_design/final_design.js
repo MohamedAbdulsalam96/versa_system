9d90959fce5be3a2359b602766aa455403edc634
NaseeraVP <naseeravp38@gmail.com>
2024-11-01 10:07:07 +0530

    fix:fix the issue

--- versa_system/versa_system/doctype/final_design/final_design.js
+++ versa_system/versa_system/doctype/final_design/final_design.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2024, efeone and contributors
+// For license information, please see license.txt
+
 frappe.ui.form.on('Final Design', {
     refresh: function(frm) {
         if (!frm.is_new() && frm.doc.workflow_state === 'Approved') {
